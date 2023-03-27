4e90320751c616bfaafc504e77cac17bd1321f81
andreaortogni <andreaortogni@gmail.com>
2023-03-27 10:36:40 +0200

    admin role middleware

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Routing\Route;
+use Inertia\Inertia;
+
+class AdminController extends Controller 
+{
+    
+    public function create()
+    {
+        return Inertia::render('Admin/Dashboard', [
+            // 'canResetPassword' => Route::has('password.request'),
+            'status' => session('status'),
+        ]);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get if user has Admin role
+     */
+    public function isAdmin()
+    {
+        return $this->is_admin;
+    }
 }

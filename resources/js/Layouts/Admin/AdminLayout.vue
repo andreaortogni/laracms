eb4bf0b716bbbd1f01bb3b9e8751805e9d8db6ee
andreaortogni <andreaortogni@gmail.com>
2023-04-04 16:01:48 +0200

    added logout button to admin layout

--- resources/js/Layouts/Admin/AdminLayout.vue
+++ resources/js/Layouts/Admin/AdminLayout.vue
@@ -1,7 +1,11 @@
 <script setup>
-
+import { Link } from '@inertiajs/vue3';
 </script>
 
 <template>
-    
+    <div id="hedaer" class="">
+        <div>
+            <Link :href="route('logout')" method="post" as="button">Logout</Link>
+        </div>
+    </div>
 </template>
